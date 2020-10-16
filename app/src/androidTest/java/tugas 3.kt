b97643f925a08a26e69fcd775390f0b306704128
unknown <you@example.com>
2020-10-16 19:33:33 +0700

    Intent

---
+++ app/src/androidTest/java/tugas 3.kt
@@ -0,0 +1,13 @@
+fun main () {
+    val umur: Int = 11
+    var suhu: Float = 32F
+    var firstName: String = "Rizqon"
+    var initial: String = "RZ"
+
+    firstName = "budi"
+
+    println(umur)
+    println(suhu)
+    println(firstName)
+    println(initial)
+}
